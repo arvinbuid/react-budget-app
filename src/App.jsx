f4e9a3ccb94cdb19e86155bfa7edbaa5a4182bb2
Arvin Buid <arvinbuid01@gmail.com>
2024-05-10 12:05:33 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className='w-screen h-screen'>
+        <div className='flex justify-center mt-8'>
+          <h1 className='text-3xl font-semibold'>React Budget App</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
