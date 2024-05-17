c1c2a13bbffaab2bb505bf9e5ba6a8cee2a24166
Arvin Buid <arvinbuid01@gmail.com>
2024-05-17 08:08:54 +0800

    Created a getAllMatchingItems() from helpers.js to get all items from local storage and store the function inside the budget variable to display the name of the category inside src/components/ExpenseItem.jsx.

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,21 @@
-import {formatCurrency, formatDate} from "../helpers";
+import {Link} from "react-router-dom";
+import {formatCurrency, formatDate, getAllMatchingItems} from "../helpers";
 
 export const ExpenseItem = ({expense}) => {
+  const budget = getAllMatchingItems({
+    category: "budgets",
+    key: "id",
+    value: expense.budgetId,
+  })[0];
+
   return (
     <>
       <td className='px-4 py-4 w-full text-center'>{expense.name}</td>
       <td className='px-4 '>{formatCurrency(expense.amount)}</td>
       <td className='px-4'>{formatDate(expense.createdAt)}</td>
-      <td className='px-4'>{formatDate(expense.createdAt)}</td>
+      <td>
+        <Link to={`/budget/${budget.id}`} ><span>{budget.name}</span></Link>
+      </td>
     </>
   );
 };
